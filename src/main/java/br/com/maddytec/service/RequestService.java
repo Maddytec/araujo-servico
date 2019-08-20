08eb42a789b9db71c2fa8765006ff6232edaeb69
Madson <maddytec@gmail.com>
2019-08-19 21:32:59 -0300

    Create RequestService.java

---
+++ src/main/java/br/com/maddytec/service/RequestService.java
@@ -0,0 +1,44 @@
+package br.com.maddytec.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.maddytec.domain.Request;
+import br.com.maddytec.domain.enums.RequestState;
+import br.com.maddytec.repository.RequestRepository;
+
+@Service
+public class RequestService {
+
+	@Autowired
+	private RequestRepository requestRepository;
+
+	public Request save(Request request) {
+		request.setState(RequestState.OPEN);
+		request.setCriationDate(new Date());
+		return requestRepository.save(request);
+	}
+
+	public Request update(Request request) {
+		return requestRepository.save(request);
+	}
+
+	public Request findById(Long id) {
+		Optional<Request> result = requestRepository.findById(id);
+		return result.get();
+	}
+
+	public List<Request> listAll() {
+		List<Request> requests = requestRepository.findAll();
+		return requests;
+	}
+
+	public List<Request> listAllByOwnerId(Long id) {
+		List<Request> requests = requestRepository.findAllByOwnerId(id);
+		return requests;
+	}
+}
