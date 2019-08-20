98c89cb87c5e0240dfa34aa1cb5ca2d77d6aa7d7
Madson <maddytec@gmail.com>
2019-08-19 22:44:51 -0300

    Create RequestStageService

---
+++ src/main/java/br/com/maddytec/service/RequestStageService.java
@@ -0,0 +1,50 @@
+package br.com.maddytec.service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.maddytec.domain.Request;
+import br.com.maddytec.domain.RequestStage;
+import br.com.maddytec.domain.enums.RequestState;
+import br.com.maddytec.repository.RequestRepository;
+import br.com.maddytec.repository.RequestStageRepository;
+
+@Service
+public class RequestStageService {
+
+	@Autowired
+	private RequestStageRepository requestStageRepository;
+
+	@Autowired
+	RequestRepository requestRepository;
+
+	public RequestStage save(RequestStage requestStage) {
+		requestStage.setRealizationDate(new Date());
+		
+		RequestStage createdStage = requestStageRepository.save(requestStage);
+		
+		Long requestId = createdStage.getRequest().getId();
+		
+		RequestState state = requestStage.getState(); 
+		
+		requestRepository.updateStatus(requestId, state);
+		
+		return createdStage;
+		
+	}
+
+	public RequestStage findById(Long id) {
+		Optional<RequestStage> result = requestStageRepository.findById(id);
+		return result.get();
+	}
+	
+	public List<RequestStage> listAllByRequestId(Long requestId){
+		List<RequestStage> stages = requestStageRepository.findAllByRequestId(requestId);
+		return stages;
+	}
+
+}
--- src/main/java/br/com/maddytec/service/UserService.java
+++ src/main/java/br/com/maddytec/service/UserService.java
@@ -13,28 +13,29 @@ import br.com.maddytec.util.HashUtil;
 @Service
 public class UserService {
 
-	@Autowired private UserRepository userReposotory;
-	
+	@Autowired
+	private UserRepository userReposotory;
+
 	public User save(User user) {
 		String hash = HashUtil.getSecureHash(user.getPassword());
-		 user.setPassword(hash);
+		user.setPassword(hash);
 		return userReposotory.save(user);
 	}
-	
+
 	public User update(User user) {
 		return userReposotory.save(user);
 	}
-	
+
 	public User findById(Long id) {
 		Optional<User> result = userReposotory.findById(id);
-		return  result.get();
+		return result.get();
 	}
-	
-	public List<User> listAll(){
+
+	public List<User> listAll() {
 		List<User> users = userReposotory.findAll();
 		return users;
 	}
-	
+
 	public User login(String email, String password) {
 		password = HashUtil.getSecureHash(password);
 		Optional<User> result = userReposotory.login(email, password);
